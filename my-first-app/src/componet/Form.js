f9be6bff1bc22f53543f9d559ca172269ce13764
alxsoftware2024 <ashenafin2024@gmail.com>
2024-11-02 19:50:03 -0700

    React form handling

--- my-first-app/src/componet/Form.js
+++ my-first-app/src/componet/Form.js
@@ -5,35 +5,61 @@ import React, { Component } from 'react'
     
       this.state = {
          userName:'',
-         comment:''
+         comment:'',
+         select:''
       }
     }
-    //Handeller
+    //userName Handeller
     userHander=(event)=>{
         this.setState({
             userName:event.target.value
         })
     }
+    //comment handler
     userComment=(event)=>{
         this.setState({
             comment:event.target.value 
         })
     }
-    
+    //select handler
+    selectHandeler=(event)=>{
+        this.setState({
+            select:event.target.value
+        })
+    }
+    //submit handler
+    submithandler=(event)=>{
+        alert(`${this.state.userName}${this.state.comment}${this.state.select}`)
+    }
   render() {
     return (
-      <div>
-        <form>
+      
+        <form onSubmit={this.submithandler}>
+            <div>
             <label>
                 UserName:
             </label>
             <input type='text' value={this.state.userName} onChange={this.userHander} />
-        <label>
+            </div>
+            
+       <div>
+       <label>
             Comment:
         </label>
         <textarea type='text' value={this.state.comment} onChange={this.userComment}/>
+       </div>
+       <div>
+        <label>SelectCourse:</label>
+        <select value={this.state.select} onChange={this.selectHandeler}>
+            <option value='Javascript'>Javascript</option>
+            <option value='React'>React</option>
+            <option value='Java'>Java</option>
+            <option value='C++'>C++</option>
+        </select>
+       </div>
+        <button type='submit'>Submit</button>
         </form>
-      </div>
+     
     )
   }
 }
