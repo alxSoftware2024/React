66ff5e016dcddfa056d4a8f934b29832fe13d5d0
alxsoftware2024 <ashenafin2024@gmail.com>
2024-11-10 20:08:45 -0800

    Reset

--- my-first-app/src/Hooks/Usestates.jsx
+++ my-first-app/src/Hooks/Usestates.jsx
@@ -7,7 +7,7 @@ const[count,setCounter]=useState(0);
 <p>Your current count is{count}</p>
 <button onClick={()=>setCounter(count+1)}>ADD</button>
 <button onClick={()=>setCounter(count-1)}>SUB</button>
-
+<button onClick={()=>setCounter(0)}>Reset</button>
     </div>
   )
 }
