c3b100dd698b22d524e6e35b38b0ba7ceb149297
alxsoftware2024 <ashenafin2024@gmail.com>
2024-11-14 03:13:27 -0800

    UseEffect

--- my-first-app/src/Hooks/Usestates3.jsx
+++ my-first-app/src/Hooks/Usestates3.jsx
@@ -1,9 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect} from 'react'
 
 function Usestates3() {
     const [Fname,setnamef]=useState('')
     const [Lname,setnamel]=useState('')
-
+     const[count,setCount]=useState(0);
+     useEffect(()=>{
+        document.title=`${count} Times clicked`
+     },[count])
   return (
 <>
 <input type='text' value={Fname} onChange={e=>setnamef(e.target.value)}/>
@@ -11,9 +14,16 @@ function Usestates3() {
 
 <h1>Your first Name:{Fname}</h1>
 <h1>Your Last Name:{Lname}</h1>
-
+<h1>Counter::{count}</h1>
+<button onClick={IncreaseCounter}>Add</button>
 </>
     )
+    function IncreaseCounter()
+    {
+       
+     return  setCount(count+1)
+       
+    }  
 }
 
 export default Usestates3
