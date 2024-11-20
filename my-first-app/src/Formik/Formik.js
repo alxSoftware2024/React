72d8e5eca8e1d5f3b9896545752280aa2713f8bb
alxsoftware2024 <ashenafin2024@gmail.com>
2024-11-19 20:38:24 -0800

    useFormik

--- my-first-app/src/Formik/Formik.js
+++ my-first-app/src/Formik/Formik.js
@@ -7,29 +7,28 @@ const initialValues={
     cpassword:'',
 };
 function Formik() {
- const [values,handleBlur,handleChange,handleSubmit]=useFormik({
+ const {values,handleBlur,handleChange,handleSubmit}=useFormik({
         initialValues,
         onSubmit:(values)=>{
             console.log(values);
         },
     });
-    console.log(formik);
   return (
     <div>
 
 {/* Form handling using Formik */}
 <form className='sign-up'>
 <label htmlFor='name'>Name:</label>
-<input type='text' name='name'/>
+<input type='text' name='name' value={values.name} onBlur={handleBlur} onChange={handleChange}/>
 <br/>
 <label htmlFor='email'>Email:</label>
-<input type='text' name='email'/>
+<input type='text' name='email'value={values.email} onBlur={handleBlur} onChange={handleChange}/>
 <br />
 <label htmlFor='password'>password:</label>
-<input type='password' name='password'/>
+<input type='password' name='password' value={values.password} onBlur={handleBlur} onChange={handleChange}/>
 <br/>
 <label htmlFor='cpassword'>C password:</label>
-<input type='password' name='cpassword'/>
+<input type='password' name='cpassword' value={values.cpassword} onBlur={handleBlur} onChange={handleChange}/>
 <br/>
 <button type='submit'>Submit</button>
 </form>
