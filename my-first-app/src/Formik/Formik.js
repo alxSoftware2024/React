d6f9bfc18b234ddafe903a10ee42c52c6d44febd
alxsoftware2024 <ashenafin2024@gmail.com>
2024-11-20 09:38:27 -0800

    Form handling

--- my-first-app/src/Formik/Formik.js
+++ my-first-app/src/Formik/Formik.js
@@ -9,12 +9,15 @@ const initialValues={
 };
 function Formik() {
     //Built in labrary in Formik
- const {values,handleBlur,handleChange,handleSubmit}=useFormik({
+ const {values,handleBlur,handleChange,handleSubmit,errors}=useFormik({
         initialValues,
-        onSubmit:(values)=>{
+        validationSchema:signUpSchemas,
+        onSubmit:(values,action)=>{
             console.log(values);
+            action.resetForm();
         },
     });
+    console.log(errors);
   return (
     <div>
 
@@ -23,15 +26,27 @@ function Formik() {
 <label htmlFor='name'>Name:</label>
 <input type='text' name='name' value={values.name} onBlur={handleBlur} onChange={handleChange}/>
 <br/>
+<div>
+  {errors.name&&(<p>{errors.name}</p>)}
+</div>
 <label htmlFor='email'>Email:</label>
 <input type='text' name='email'value={values.email} onBlur={handleBlur} onChange={handleChange}/>
 <br />
+<div>
+  {errors.email&&(<p>{errors.email}</p>)}
+</div>
 <label htmlFor='password'>password:</label>
 <input type='password' name='password' value={values.password} onBlur={handleBlur} onChange={handleChange}/>
 <br/>
+<div>
+  {errors.password&&(<p>{errors.password}</p>)}
+</div>
 <label htmlFor='cpassword'>C password:</label>
 <input type='password' name='cpassword' value={values.cpassword} onBlur={handleBlur} onChange={handleChange}/>
 <br/>
+<div>
+  {errors.cpassword&&(<p>{errors.cpassword}</p>)}
+</div>
 <button type='submit'>Submit</button>
 </form>
 
--- my-first-app/src/schemas/index.js
+++ my-first-app/src/schemas/index.js
@@ -2,7 +2,7 @@ import * as Yup from 'yup'
 const strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
 
 export const signUpSchemas=Yup.object({
-   name: Yup.string().min(3).required("Please Enter values"),
+   name: Yup.string().min(3).required("Please Enter name"),
    email:Yup.string().email("Please enter valid email").required("please enter email"),
    password:Yup.string().matches(strongRegex,"Please Enter valid password").required("please enter password"),
    cpassword:Yup.string().oneOf([Yup.ref("password")],"Password Not Match").required("please enter Confrim password"),
